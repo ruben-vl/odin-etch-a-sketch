f003692940fa5713f4ab3e3556cd091b3a759820
ruben-vl <rubenvanlaer@mail.com>
2024-03-09 16:22:16 +0100

    Sketch grid with nonworking newgrid

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-function createGrid(dim) {
-    existing_children = document.querySelector("sketch-grid > div");
-    for (child in existing_children) {
-        sketch_grid.removeChild(child)
-    }
-    for (let i = 0; i < dim; i++) {
-        for (let j = 0; j < dim; j++) {
-            const div = document.createElement('div');
-            div.style.width = `${100/dim}%`;
-            div.style.height = "auto";
-            div.style.maxWidth = `${80/dim}vw`;
-            div.style.maxHeight = `${70/dim}vh`;
-
-            div.addEventListener('mouseover', (e) => {
-                if (e.buttons == 1) {
-                    e.target.classList.toggle("black-background");
-                }
-            });
-            div.addEventListener('click', (e) => {
-                e.target.classList.toggle("black-background");
-            })
-            sketch_grid.appendChild(div);
-        }
-    }
-}
-
 let window_width = window.innerWidth;
 let window_height = window.innerHeight;
 
@@ -41,17 +15,37 @@ if (0.7*window_height < 0.8*window_width) {
 }
 
 let grid_dimension = 16;
-
-let primaryMouseButtonDown = false;
-
-function setPrimaryButtonState(e) {
-  var flags = e.buttons !== undefined ? e.buttons : e.which;
-  primaryMouseButtonDown = (flags & 1) === 1;
-}
-
-document.addEventListener("mousedown", setPrimaryButtonState);
-document.addEventListener("mousemove", setPrimaryButtonState);
-document.addEventListener("mouseup", setPrimaryButtonState);
-
 createGrid(grid_dimension);
+
+const new_grid_button = document.querySelector("#newgrid");
+
+new_grid_button.addEventListener('click', (e) => {
+    let gridSize = parseInt(prompt("New Grid Size ([1,100]):"));
+    if (gridSize != NaN && gridSize >= 1 && gridSize <= 100) {
+        createGrid(gridSize);
+    } else {
+        alert("Not a valid grid size. The size needs to be between 1 and 100, both inclusive.");
+    }
+})
+
+function createGrid(dim) {
+    existing_children = document.querySelector("sketch-grid > div");
+    for (child in existing_children) {
+        sketch_grid.removeChild(child);
+    }
+    for (let i = 0; i < dim; i++) {
+        for (let j = 0; j < dim; j++) {
+            const div = document.createElement('div');
+            div.style.width = `${100/dim}%`;
+            div.style.height = "auto";
+            div.style.maxWidth = `${80/dim}vw`;
+            div.style.maxHeight = `${70/dim}vh`;
+
+            div.addEventListener('mouseover', (e) => {
+                e.target.classList.toggle("black-background");
+            });
+            sketch_grid.appendChild(div);
+        }
+    }
+}
 
